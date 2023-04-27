3a2c97ed316776b620f1958b1886dacd304cb076
Tunahan Akdogan <tuna_han1999@hotmail.com>
2023-04-27 17:04:55 +0300

    necessary files for village class created

---
+++ Farmer_Data_Entry_API/Controllers/VillagesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Farmer_Data_Entry_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillagesController : ControllerBase
+    {
+    }
+}
---
+++ Farmer_Data_Entry_API/Repositories/VillageRepo.cs
@@ -0,0 +1,6 @@
+﻿namespace Farmer_Data_Entry_API.Repositories
+{
+    public class VillageRepo: IVillageRepo
+    {
+    }
+}
