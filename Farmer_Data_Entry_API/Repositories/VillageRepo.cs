f4c57ce3aa949f0fa60fb8224bbb7eb3a93ca7f6
Tunahan Akdogan <tuna_han1999@hotmail.com>
2023-04-27 17:21:13 +0300

    Village repository functions created but not filled

--- Farmer_Data_Entry_API/Repositories/VillageRepo.cs
+++ Farmer_Data_Entry_API/Repositories/VillageRepo.cs
@@ -1,6 +1,33 @@
-﻿namespace Farmer_Data_Entry_API.Repositories
+﻿using Farmer_Data_Entry_API.DTOs;
+using Farmer_Data_Entry_API.Entities;
+
+namespace Farmer_Data_Entry_API.Repositories
 {
-    public class VillageRepo: IVillageRepo
+    public class VillageRepo : IVillageRepo
     {
+        public Task<Village> CreateVillage(VillageDTO company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteVillage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Village> GetVillage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Village>> GetVillages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateVillage(int id, VillageDTO company)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
