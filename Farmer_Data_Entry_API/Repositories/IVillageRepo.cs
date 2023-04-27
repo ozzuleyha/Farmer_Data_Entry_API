1b458ecd8b29f7df7a452efd410d9b7101f38f71
Tunahan Akdogan <tuna_han1999@hotmail.com>
2023-04-27 17:06:36 +0300

    village repository interface created

--- Farmer_Data_Entry_API/Repositories/IVillageRepo.cs
+++ Farmer_Data_Entry_API/Repositories/IVillageRepo.cs
@@ -1,6 +1,14 @@
-﻿namespace Farmer_Data_Entry_API.Repositories
+﻿using Farmer_Data_Entry_API.DTOs;
+using Farmer_Data_Entry_API.Entities;
+
+namespace Farmer_Data_Entry_API.Repositories
 {
     public interface IVillageRepo
     {
+        public Task<IEnumerable<Village>> GetVillages();
+        public Task<Village> GetVillage(int id);
+        public Task UpdateVillage(int id, VillageDTO company);
+        public Task DeleteVillage(int id);
+        public Task<Village> CreateVillage(VillageDTO company);
     }
 }
